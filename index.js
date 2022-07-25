4e747dd996e8a484daa015660d6f5edb0be154eb
Mikael Vesavuori <mikaelvesavuori@gmail.com>
2022-07-25 19:47:57 +0000

    update

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-let express = require('express');
-let app = express();
-let ejs = require('ejs');
-const haikus = require('./haikus.json');
-const port = process.env.PORT || 3000;
-
-app.use(express.static('public'))
-app.set('view engine', 'ejs');
-
-app.get('/', (req, res) => {
-  res.render('index', {haikus: haikus});
-});
-
-app.listen(port);
+import { MikroLog } from "mikrolog";
+const logger = MikroLog.start();
+logger.log("Hello World!");
